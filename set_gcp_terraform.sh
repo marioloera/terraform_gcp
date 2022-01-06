2563d5406edf41b6440ad72f097811cde52cef8b
Mario Loera <mario.loera@trustly.com>
2022-01-06 13:21:18 -0700

    add generic gcp_variables scprits

---
+++ set_gcp_terraform.sh
@@ -0,0 +1,4 @@
+export TF_VAR_gcp_project="chrome-bearing-336918"
+export TF_VAR_gcp_credentials_file="../gcp_keys/chrome-bearing-336918-09064f2caabd_terraform.json"
+echo $TF_VAR_gcp_project
+echo $TF_VAR_gcp_credentials_file
