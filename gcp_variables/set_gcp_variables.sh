5ebb101a1fb8016460f5174f0e42cbb7efd2e06c
Mario Loera <mario.loera@trustly.com>
2022-01-06 13:31:50 -0700

    move gcp var files files

--- gcp_variables/set_gcp_variables.sh
+++ gcp_variables/set_gcp_variables.sh
@@ -1,3 +1,5 @@
+# run:
+# source ./gcp_keys/set_gcp_variables.sh
 export TF_VAR_gcp_project="chrome-bearing-336918"
 export TF_VAR_gcp_credentials_file="../gcp_keys/chrome-bearing-336918-09064f2caabd_terraform.json"
 echo $TF_VAR_gcp_project
