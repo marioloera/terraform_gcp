3d1344bdd02c7a6324619e64d9e3ed6297f00454
Mario Loera <mario.loera@trustly.com>
2022-01-06 13:21:42 -0700

    add  & use gcp variables in confing

--- KubernetesLoadBalancer/main.tf
+++ KubernetesLoadBalancer/main.tf
@@ -11,7 +11,9 @@ variable "network_name" {
 }
 
 provider "google" {
-  region = var.region
+  credentials = file(var.gcp_credentials_file)
+  project     = var.gcp_project
+  region      = var.region
 }
 
 resource "google_compute_network" "default" {
