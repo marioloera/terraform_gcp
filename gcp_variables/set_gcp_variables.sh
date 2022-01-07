124fbe0a6d85d49a6e788411b28f6ff08de45d87
Mario Loera <mario.loera@trustly.com>
2022-01-06 18:23:37 -0700

    add project besides gcp_project in set_gcp_variables script

--- gcp_variables/set_gcp_variables.sh
+++ gcp_variables/set_gcp_variables.sh
@@ -1,6 +1,7 @@
 # run:
-# source ./gcp_keys/set_gcp_variables.sh
+# source ./gcp_variables/set_gcp_variables.sh
 export TF_VAR_gcp_project="chrome-bearing-336918"
+export TF_VAR_project=$TF_VAR_gcp_project
 export TF_VAR_gcp_credentials_file="../gcp_keys/chrome-bearing-336918-09064f2caabd_terraform.json"
 echo $TF_VAR_gcp_project
 echo $TF_VAR_gcp_credentials_file
