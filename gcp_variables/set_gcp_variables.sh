030ee777f1904aa94c9c0a4bab0583ae60137dd8
Mario Loera <mario.loera@trustly.com>
2022-01-07 00:12:00 -0700

    fix set_gcp_variables script and gitignore with correct dir name

--- gcp_variables/set_gcp_variables.sh
+++ gcp_variables/set_gcp_variables.sh
@@ -2,6 +2,6 @@
 # source ./gcp_variables/set_gcp_variables.sh
 export TF_VAR_gcp_project="chrome-bearing-336918"
 export TF_VAR_project=$TF_VAR_gcp_project
-export TF_VAR_gcp_credentials_file="../gcp_keys/chrome-bearing-336918-09064f2caabd_terraform.json"
+export TF_VAR_gcp_credentials_file="../gcp_variables/chrome-bearing-336918-09064f2caabd_terraform.json"
 echo $TF_VAR_gcp_project
 echo $TF_VAR_gcp_credentials_file
